70460e574c8fb484bbf6c6846920d0b8fd423d96
Cydiater <cydiater@gmail.com>
2022-07-21 22:25:55 +0800

    fix run_all.py

--- scripts/run_all.py
+++ scripts/run_all.py
@@ -5,21 +5,23 @@ import os
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data_dir', default='./testcases/functional')
+    parser.add_argument('--data_dir', default='./testcases/')
     parser.add_argument('--compiler_path', default='./build/compiler')
     args = parser.parse_args()
     return args
 
 
 def run_with(args):
-    for filename in os.listdir(args.data_dir):
-        if not filename.endswith(".sy"):
-            continue
-        full_path = os.path.join(args.data_dir, filename)
-        asm_path = full_path[:-1]
-        cmd = "{} {} -o {}".format(args.compiler_path, full_path, asm_path)
-        print(cmd)
-        subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
+    for mod in os.listdir(args.data_dir):
+        mod_path = os.path.join(args.data_dir, mod)
+        for filename in os.listdir(mod_path):
+            if not filename.endswith(".sy"):
+                continue
+            full_path = os.path.join(mod_path, filename)
+            asm_path = full_path[:-1]
+            cmd = "{} {} -o {}".format(args.compiler_path, full_path, asm_path)
+            print(cmd)
+            subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
 
 
 if __name__ == '__main__':
