a0bff3137d060ef45d969af865a655c27505e4bd
Cydiater <cydiater@gmail.com>
2022-07-24 00:56:20 +0800

    fix: use indirect map

--- src/ir/opt/mem2reg.cpp
+++ src/ir/opt/mem2reg.cpp
@@ -3,26 +3,32 @@
 #include "ir/opt/opt.hpp"
 
 void mem2reg_func(IR::NormalFunc *func) {
-  std::unordered_map<IR::Reg, IR::Reg> addr2value;
+  std::unordered_map<IR::Reg, IR::MemObject *> addr2mem;
+  std::unordered_map<IR::MemObject *, IR::Reg> mem2value;
   func->for_each([&](IR::BB *bb) {
     for (auto it = bb->instrs.begin(); it != bb->instrs.end(); it++) {
       if (auto load_addr_instr = dynamic_cast<IR::LoadAddr *>(it->get())) {
         auto mem_obj = load_addr_instr->offset;
         if (!mem_obj->global && mem_obj->is_single_var()) {
-          if (addr2value.find(load_addr_instr->d1) == addr2value.end()) {
+          if (mem2value.find(mem_obj) == mem2value.end()) {
             auto value_reg = func->new_Reg(mem_obj->name);
-            addr2value.insert({load_addr_instr->d1, value_reg});
+            mem2value.insert({mem_obj, value_reg});
           }
+          addr2mem.insert({load_addr_instr->d1, mem_obj});
         }
       } else if (auto load_instr = dynamic_cast<IR::LoadInstr *>(it->get())) {
-        if (addr2value.find(load_instr->addr) != addr2value.end()) {
-          auto value_reg = addr2value[load_instr->addr];
+        if (addr2mem.find(load_instr->addr) != addr2mem.end()) {
+          auto mem_obj = addr2mem[load_instr->addr];
+          assert(mem2value.find(mem_obj) != mem2value.end());
+          auto value_reg = mem2value[mem_obj];
           *it = std::make_unique<IR::UnaryOpInstr>(load_instr->d1, value_reg,
                                                    IR::UnaryOp::ID);
         }
       } else if (auto store_instr = dynamic_cast<IR::StoreInstr *>(it->get())) {
-        if (addr2value.find(store_instr->addr) != addr2value.end()) {
-          auto value_reg = addr2value[store_instr->addr];
+        if (addr2mem.find(store_instr->addr) != addr2mem.end()) {
+          auto mem_obj = addr2mem[store_instr->addr];
+          assert(mem2value.find(mem_obj) != mem2value.end());
+          auto value_reg = mem2value[mem_obj];
           *it = std::make_unique<IR::UnaryOpInstr>(value_reg, store_instr->s1,
                                                    IR::UnaryOp::ID);
         }
