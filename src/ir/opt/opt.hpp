89a86a695125a6ad10198c5c8e570df274f2fc79
cYd1αtεr <cydiater@gmail.com>
2022-07-30 00:47:29 +0800

    opt(IR): introduce Global Value Numbering (#79)

--- src/ir/opt/opt.hpp
+++ src/ir/opt/opt.hpp
@@ -6,10 +6,14 @@ void mem2reg(IR::CompileUnit *);
 void ssa_construction(IR::NormalFunc *, const std::unordered_set<IR::Reg> &);
 void remove_unused_def(IR::CompileUnit *);
 void global_code_motion(IR::CompileUnit *);
+void global_value_numbering(IR::CompileUnit *);
 
 inline void optimize_ir(IR::CompileUnit *ir) {
   mem2reg(ir);
   remove_unused_def(ir);
+  if (global_config.disabled_passes.find("gvn") ==
+      global_config.disabled_passes.end())
+    global_value_numbering(ir);
   if (global_config.disabled_passes.find("gvm") ==
       global_config.disabled_passes.end())
     global_code_motion(ir);
