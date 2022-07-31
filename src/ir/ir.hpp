4cc2a345f82fa15f36ff8f34816f7721c8e0c61e
Cydiater <cydiater@gmail.com>
2022-07-31 16:50:00 +0800

    maybe unused

--- src/ir/ir.hpp
+++ src/ir/ir.hpp
@@ -595,8 +595,7 @@ struct PhiInstr : RegWriteInstr {
   void print(ostream &os) const override;
 };
 
-#define Case(T, a, b)                                                          \
-  if (auto __attribute__((warn_unused_result)) a = dynamic_cast<T *>(b))
+#define Case(T, a, b) if ([[maybe_unused]] auto a = dynamic_cast<T *>(b))
 #define CaseNot(T, b)                                                          \
   if (auto _ = dynamic_cast<T *>(b)) {                                         \
   } else
