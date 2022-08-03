35a78207f268d040fd4e700903b7b9f88e28fa79
王建楠 <wangjn19@mails.tsinghua.edu.cn>
2022-08-03 23:36:59 +0800

    all with inline $bench

--- src/ir/opt/opt.hpp
+++ src/ir/opt/opt.hpp
@@ -45,17 +45,14 @@ inline void optimize_ir(IR::CompileUnit *ir) {
       gvn(ir);
       call_graph(ir);
       gvn(ir);
-      /*
-      PassEnabled("func-inline") {
-	func_inline(ir);
-        // PassEnabled("g2l") global_to_local(ir);
-        // mem2reg(ir);
-        remove_unused_func(ir);
-        gvn(ir);
-        dag_ir(ir);
-        gvn(ir);
-        gcm(ir);
-      }*/
+      func_inline(ir);
+      // PassEnabled("g2l") global_to_local(ir);
+      // mem2reg(ir);
+      remove_unused_func(ir);
+      gvn(ir);
+      dag_ir(ir);
+      gvn(ir);
+      gcm(ir);
     }
   }
   PassEnabled("del-phi") before_backend(ir);
