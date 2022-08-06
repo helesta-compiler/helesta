1e96499f833383c96afc0d9da29fe9480eb88fe0
Cydiater <cydiater@gmail.com>
2022-08-06 21:49:43 +0800

    upd

--- src/ir/opt/opt.hpp
+++ src/ir/opt/opt.hpp
@@ -11,11 +11,12 @@ void mem2reg(IR::CompileUnit *);
 void ssa_construction(IR::NormalFunc *, const std::unordered_set<IR::Reg> &);
 void remove_unused_def(IR::CompileUnit *);
 void remove_unused_def_func(IR::NormalFunc *);
-void func_inline(IR::CompileUnit *);
 void global_code_motion(IR::CompileUnit *);
 void global_value_numbering(IR::CompileUnit *);
 void global_to_local(IR::CompileUnit *);
 void before_backend(IR::CompileUnit *ir);
+void call_graph(IR::CompileUnit *ir);
+void remove_unused_BB(IR::CompileUnit *ir);
 
 inline void gvn(IR::CompileUnit *ir) {
   PassEnabled("gvn") global_value_numbering(ir);
@@ -30,9 +31,17 @@ inline void optimize_ir(IR::CompileUnit *ir) {
   PassEnabled("opt") {
     PassEnabled("mem2reg") mem2reg(ir);
     remove_unused_def(ir);
+    remove_unused_BB(ir);
     gvn(ir);
     gcm(ir);
     gvn(ir);
+    PassEnabled("misc") {
+      call_graph(ir);
+      dag_ir(ir);
+      gvn(ir);
+      call_graph(ir);
+      gvn(ir);
+    }
   }
   PassEnabled("del-phi") before_backend(ir);
 }
