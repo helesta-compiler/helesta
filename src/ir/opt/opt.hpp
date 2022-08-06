befcb27e7a2d2263652b9c7a7f9386b3d1dc77b1
Cydiater <cydiater@gmail.com>
2022-08-07 00:43:07 +0800

    fix

--- src/ir/opt/opt.hpp
+++ src/ir/opt/opt.hpp
@@ -11,16 +11,20 @@ void mem2reg(IR::CompileUnit *);
 void ssa_construction(IR::NormalFunc *, const std::unordered_set<IR::Reg> &);
 void remove_unused_def(IR::CompileUnit *);
 void remove_unused_def_func(IR::NormalFunc *);
+void remove_unused_func(IR::CompileUnit *);
+void func_inline(IR::CompileUnit *);
 void global_code_motion(IR::CompileUnit *);
 void global_value_numbering(IR::CompileUnit *);
 void global_to_local(IR::CompileUnit *);
-void before_backend(IR::CompileUnit *ir);
-void call_graph(IR::CompileUnit *ir);
+void simplify_expr(IR::CompileUnit *ir);
+void call_graph(IR::CompileUnit *);
 void remove_unused_BB(IR::CompileUnit *ir);
+void before_backend(IR::CompileUnit *ir);
 
 inline void gvn(IR::CompileUnit *ir) {
   PassEnabled("gvn") global_value_numbering(ir);
   remove_unused_def(ir);
+  PassEnabled("se") simplify_expr(ir);
 }
 
 inline void gcm(IR::CompileUnit *ir) {
@@ -41,6 +45,16 @@ inline void optimize_ir(IR::CompileUnit *ir) {
       gvn(ir);
       call_graph(ir);
       gvn(ir);
+      PassEnabled("func-inline") {
+        func_inline(ir);
+        // PassEnabled("g2l") global_to_local(ir);
+        // mem2reg(ir);
+        remove_unused_func(ir);
+        gvn(ir);
+        dag_ir(ir);
+        gvn(ir);
+        gcm(ir);
+      }
     }
   }
   PassEnabled("del-phi") before_backend(ir);
